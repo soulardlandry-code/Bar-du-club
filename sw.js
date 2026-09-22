5a0a15f9e2caf66eace6b29b78f3f172b06301f8
soulardlandry-code <soulard.landry@gmail.com>
2026-09-22 10:21:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-Unsupported Media Type
+const CACHE_NAME="bar-du-club-v5";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./logo-aph.jpg","./biere.jpg","./vin.jpg","./petit_soda.jpg","./grand_soda.jpg","./sandwich.jpg"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{let copy=r.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
